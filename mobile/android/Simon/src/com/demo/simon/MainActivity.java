bee5516fad352bea4268b425e9883e66e00caf26
tomdong <jiang2913@hotmail.com>
2014-12-29 21:02:01 +0800

    Add location util.

--- mobile/android/Simon/src/com/demo/simon/MainActivity.java
+++ mobile/android/Simon/src/com/demo/simon/MainActivity.java
@@ -3,23 +3,43 @@ package com.demo.simon;
 
 import android.app.Activity;
 import android.app.FragmentTransaction;
+import android.location.Location;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.Window;
 import android.widget.Button;
 
+import com.amap.api.location.AMapLocation;
+import com.amap.api.location.AMapLocationListener;
+import com.amap.api.location.LocationManagerProxy;
+import com.amap.api.location.LocationProviderProxy;
+import com.demo.simon.utility.LocationUtility;
 import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements
+AMapLocationListener, Runnable {
     private MyOrderFragment mOrderFragment;
     private SendExpressFragment mSendExpressFragment;
     private String mCurrentFragmentTag;
 
+    private LocationManagerProxy aMapLocManager = null;
+	private AMapLocation aMapLocation;
+	private Handler handler = new Handler();
+	
+	private static MainActivity instance = null;
+	
+	public static MainActivity getInstance()
+	{
+		return instance;
+	}
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        instance = this;
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         // set the content view
         setContentView(R.layout.activity_main);
@@ -86,6 +106,16 @@ public class MainActivity extends Activity {
             }
 
         });
+        
+        initLocationService();
+    }
+    
+    private void initLocationService()
+    {
+		aMapLocManager = LocationManagerProxy.getInstance(this);
+		aMapLocManager.requestLocationUpdates(
+				LocationProviderProxy.AMapNetwork, 2000, 10, this);
+		handler.postDelayed(this, 20000);
     }
 
     @Override
@@ -94,4 +124,75 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
+
+	@Override
+	public void onLocationChanged(Location location)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onStatusChanged(String provider, int status, Bundle extras)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onProviderEnabled(String provider)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onProviderDisabled(String provider)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void run()
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onLocationChanged(AMapLocation location)
+	{
+		if (location != null) {
+			this.aMapLocation = location;
+			Double geoLat = location.getLatitude();
+			Double geoLng = location.getLongitude();
+			String cityCode = "";
+			String desc = "";
+			Bundle locBundle = location.getExtras();
+			if (locBundle != null) {
+				cityCode = locBundle.getString("citycode");
+				desc = locBundle.getString("desc");
+			}
+			LocationUtility.locationString = geoLng + "," + geoLat;
+			LocationUtility.street = LocationUtility.setStreetName(location.getStreet());
+			LocationUtility.district = location.getDistrict();
+			LocationUtility.city = location.getCity();
+			LocationUtility.address = location.getAddress();
+		}
+	}
+	
+	@Override
+	protected void onPause() {
+		super.onPause();
+		stopLocation();
+	}
+	
+	private void stopLocation() {
+		if (aMapLocManager != null) {
+			aMapLocManager.removeUpdates(this);
+			aMapLocManager.destory();
+		}
+		aMapLocManager = null;
+	}
 }
